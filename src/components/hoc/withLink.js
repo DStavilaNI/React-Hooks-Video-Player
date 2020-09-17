8d83b6f9d9ecd7ed827cf6ab7e3c943aa52d1410
XenderMd <denis.stavilamd@gmail.com>
2020-09-17 14:54:11 +0200

    Final Save operation :)

--- src/components/hoc/withLink.js
+++ src/components/hoc/withLink.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const withLink = WrappedComponent =>props => {
+const withLink = WrappedComponent => props => {
     const newProps = {
         ...props,
         video:{
